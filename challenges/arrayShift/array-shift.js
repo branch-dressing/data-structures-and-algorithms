dc4f5859eff8258e7691550c7aed730f6837aa0b
joelpdurham <joeldur@gmail.com>
2019-12-03 21:02:30 -0800

    wrote more fringe tests. Now they all pass.

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,5 +1,6 @@
 function insertShiftArray(array, valueToBeAdded) {
     if(array.length === 0) return [valueToBeAdded];
+
     let returnArray = [];
     let x = 0;
     const indexToAddAt = Math.ceil(array.length / 2) -  1;
@@ -15,6 +16,7 @@ function insertShiftArray(array, valueToBeAdded) {
 }
 
 function removeMiddle(array) {
+    if(array.length === 0) return 'Nothing to remove';
     const indexToDelete = Math.ceil(array.length / 2) - 1;
     let returnArray = [];
     let x = 0;
