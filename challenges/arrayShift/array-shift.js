ca4a116dc0d2dcb7032cadeb0ce02b63a22c8e76
joelpdurham <joeldur@gmail.com>
2019-12-03 17:47:58 -0800

    started stretch, but then had to teach 3 other people how to solve this problem

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,7 +1,7 @@
 function insertShiftArray(array, valueToBeAdded) {
     let returnArray = new Array(array.length + 1);
     let x = 0;
-    const indexToAddAt = Math.ceil(array.length / 2) - 1;
+    const indexToAddAt = Math.ceil(array.length / 2) -  1;
 
     for(let i = 0; i < array.length; i++) {
         returnArray[x] = array[i];
@@ -11,10 +11,19 @@ function insertShiftArray(array, valueToBeAdded) {
             x++;
         }
     }
-
     return returnArray;
 }
 
+function removeMiddle(array) {
+    const indexToDelete = Math.ceil(array.length / 2) - 1;
+
+    for(let i = 0; i < array.length - 1; i++) {
+
+    }
+
+}
+
 module.exports = {
-    insertShiftArray
+    insertShiftArray,
+    removeMiddle
 };
