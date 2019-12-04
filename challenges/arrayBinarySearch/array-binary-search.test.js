89d787f4db603cc6ef131d2a15a64b43e872d322
joelpdurham <joeldur@gmail.com>
2019-12-04 14:24:29 -0800

    tested case with strings. still works

--- challenges/arrayBinarySearch/array-binary-search.test.js
+++ challenges/arrayBinarySearch/array-binary-search.test.js
@@ -7,6 +7,8 @@ describe('Tests Array Binary Search', () => {
         expect(binarySearch([1], 1)).toEqual(0);
         expect(binarySearch([4, 8, 15, 16, 23, 42], 15)).toEqual(2);
         expect(binarySearch([11, 22, 33, 44, 55, 66, 77], 90)).toEqual(-1);
+        expect(binarySearch([], 10)).toEqual(-1);
+        expect(binarySearch(['Does', 'it', 'work', 'with', 'strings?'], 'work')).toEqual(2);
     });
 });
 
