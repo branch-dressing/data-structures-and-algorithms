4c20adb27cb6a4d6687302af6af215d12ba5f8fa
joelpdurham <joeldur@gmail.com>
2019-12-03 17:05:56 -0800

    whiteboarding bitches

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,5 +1,9 @@
 function insertShiftArray(array, valueToBeAdded) {
-
+    //WHITEBOARDING
+    //take array.length divided by two to get the half way point, subtract 1 to get the correct index. Save to variable
+    //loop through array, creating a new array almost identical to old array.
+    //when i = variable from two lines ago, instead of adding old index of array, add valuetobeadded.
+    //continue looping through old array adding the numbers.
 
 
     return returnArray;
