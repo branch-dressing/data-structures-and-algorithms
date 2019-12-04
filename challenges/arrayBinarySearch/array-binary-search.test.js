5d499dc45063531d81568a2dd8c93a5f7288bf89
joelpdurham <joeldur@gmail.com>
2019-12-04 14:22:33 -0800

    another passing test

--- challenges/arrayBinarySearch/array-binary-search.test.js
+++ challenges/arrayBinarySearch/array-binary-search.test.js
@@ -5,7 +5,8 @@ const {
 describe('Tests Array Binary Search', () => {
     it('takes array and search value, returns index of searched item', () => {
         expect(binarySearch([1], 1)).toEqual(0);
-        expect(binarySearch([4,8,15,16,23,42], 15)).toEqual(2);
+        expect(binarySearch([4, 8, 15, 16, 23, 42], 15)).toEqual(2);
+        expect(binarySearch([11, 22, 33, 44, 55, 66, 77], 90)).toEqual(-1);
     });
 });
 
