2ee90c65124e49792ab8ee231eb0d143c5e4abcc
joelpdurham <joeldur@gmail.com>
2019-12-05 22:15:27 -0800

    refactored binary search

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,20 +1,13 @@
 function binarySearch(array, searchKey) {
-    let halfwayIndex = Math.floor(array.length / 2);
     let topLimit = array.length;
     let bottomLimit = 0;
     
-    while(topLimit - bottomLimit >= 0) {
-        if(array[halfwayIndex] === searchKey) {
-            return halfwayIndex;
-        } else if(array[halfwayIndex] > searchKey) {
-            topLimit = halfwayIndex;
-            
-        } else if(array[halfwayIndex] < searchKey) {
-            bottomLimit = halfwayIndex; 
-        }
-        halfwayIndex = Math.floor((bottomLimit + topLimit) / 2);
-    }
-    
+    for(let i = 0; i < array.length / 2; i++) {
+        let halfwayIndex = Math.floor((bottomLimit + topLimit) / 2);
+        if(array[halfwayIndex] === searchKey) return halfwayIndex;
+        if(array[halfwayIndex] > searchKey) topLimit = halfwayIndex;
+        if(array[halfwayIndex] < searchKey) bottomLimit = halfwayIndex; 
+    } 
     return -1;
 }
 
