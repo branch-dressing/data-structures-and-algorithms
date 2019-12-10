7cfa4cff5bf72836510aaf0208d381ea11cf3c93
joelpdurham <joeldur@gmail.com>
2019-12-09 16:49:20 -0800

    wrote test for insert

--- challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
+++ challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
@@ -13,22 +13,25 @@ describe('Doubly Linked-List tests', () => {
     const myDoubleLink = new DoublyLinkedList(0);
     myDoubleLink.insert(1);
     myDoubleLink.insert(2);
-    myDoubleLink.insert(3);
-    myDoubleLink.insert(4);
 
     describe('Doubly Linked List', () => {
         it('have corrert previousNode value', () => {
-            expect(myDoubleLink.head.next.value).toEqual(3);
-            expect(myDoubleLink.head.next.next.previous.value).toEqual(3);
-            expect(myDoubleLink.head.next.next.next.next.previous.value).toEqual(1);
-            expect(myDoubleLink.head.next.next.next.next.previous.previous.previous.previous.value).toEqual(4);
+            expect(myDoubleLink.head.next.value).toEqual(1);
+            expect(myDoubleLink.head.next.next.previous.value).toEqual(1);
+            expect(myDoubleLink.head.next.next.previous.previous.value).toEqual(2);
         });
     });
 
     describe('Extended Double Link tests', () => {
         it('appends a node to the end of the list', () => {
-            myDoubleLink.append(10);
-            expect(myDoubleLink.head.next.next.next.next.next.value).toEqual(10);
+            myDoubleLink.append('x');
+            expect(myDoubleLink.head.next.next.next.value).toEqual('x');
         });
+        it('can insert a new node somewhere in the list', () => {
+            expect(myDoubleLink.head.next.value).toEqual(1);
+            myDoubleLink.insertBefore(1, 'x');
+            expect(myDoubleLink.head.next.value).toEqual('x');
+        });
+
     });
 });
