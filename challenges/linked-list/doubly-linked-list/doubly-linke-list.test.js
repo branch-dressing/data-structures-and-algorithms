b138506e0db09febb059935c527c8fa9305030dd
joelpdurham <joeldur@gmail.com>
2019-12-09 16:35:29 -0800

    wrote test for append

--- challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
+++ challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
@@ -10,18 +10,25 @@ describe('Doubly Linked-List tests', () => {
             expect(testNode0).toEqual({ value: 0 });
         });
     });
+    const myDoubleLink = new DoublyLinkedList(0);
+    myDoubleLink.insert(1);
+    myDoubleLink.insert(2);
+    myDoubleLink.insert(3);
+    myDoubleLink.insert(4);
 
     describe('Doubly Linked List', () => {
-        const myDoubleLink = new DoublyLinkedList(0);
         it('have corrert previousNode value', () => {
-            myDoubleLink.insert(1);
-            myDoubleLink.insert(2);
-            myDoubleLink.insert(3);
-            myDoubleLink.insert(4);
             expect(myDoubleLink.head.next.value).toEqual(3);
             expect(myDoubleLink.head.next.next.previous.value).toEqual(3);
             expect(myDoubleLink.head.next.next.next.next.previous.value).toEqual(1);
             expect(myDoubleLink.head.next.next.next.next.previous.previous.previous.previous.value).toEqual(4);
+        });
+    });
+
+    describe('Extended Double Link tests', () => {
+        it('appends a node to the end of the list', () => {
+            myDoubleLink.append('append me');
+            expect(myDoubleLink.head.next.next.next.next.next.next.value).toEqual('append me');
         });
     });
 });
