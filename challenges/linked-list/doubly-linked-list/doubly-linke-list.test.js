9ad143786f0b1293b550439456068097b3a2ffbf
joelpdurham <joeldur@gmail.com>
2019-12-09 20:57:30 -0800

    did a bunch of fringe test

--- challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
+++ challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
@@ -4,12 +4,14 @@ const {
 } = require('./doubly-linked-list');
 
 describe('Doubly Linked-List tests', () => {
+
     describe('Node Class Tests', () => {
         const testNode0 = new Node(0);
         it('Still creates a new node with correct value', () => {
             expect(testNode0).toEqual({ value: 0 });
         });
     });
+
     const myDoubleLink = new DoublyLinkedList(0);
     myDoubleLink.insert(1);
     myDoubleLink.insert(2);
@@ -25,23 +27,50 @@ describe('Doubly Linked-List tests', () => {
     describe('Extended Double Link tests', () => {
         it('appends a node to the end of the list', () => {
             myDoubleLink.append('a');
-            expect(myDoubleLink.toString()).toEqual([2, 1, 0, 'a']);
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-2->1', '2<-1->0', '1<-0->a', '0<-a->null']);
         });
+
         it('can insert a new node before a value', () => {
             myDoubleLink.insertBefore(1, 'b');
-            expect(myDoubleLink.toString()).toEqual([2, 'b', 1, 0, 'a']);
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-2->b', '2<-b->1', 'b<-1->0', '1<-0->a', '0<-a->null']);
         });
+
+        it('can insert a new node before the head', () => {
+            myDoubleLink.insertBefore(2, 'x');
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-x->2', 'x<-2->b', '2<-b->1', 'b<-1->0', '1<-0->a', '0<-a->null']);
+        });
+
         it('can insert a new node after a value', () => {
             myDoubleLink.insertAfter(1, 'c');
-            expect(myDoubleLink.toString()).toEqual([2, 'b', 1, 'c', 0, 'a']);
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-x->2', 'x<-2->b', '2<-b->1', 'b<-1->c', '1<-c->0', 'c<-0->a', '0<-a->null']);
         });
+        
+        it('can insert a new node after the end', () => {
+            myDoubleLink.insertAfter('a', 'z');
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-x->2', 'x<-2->b', '2<-b->1', 'b<-1->c', '1<-c->0', 'c<-0->a', '0<-a->z', 'a<-z->null']);
+        });
+
         it('can delete a node given a value', () => {
             myDoubleLink.delete(1);
-            expect(myDoubleLink.toString()).toEqual([2, 'b', 'c', 0, 'a']);
-            myDoubleLink.delete(0);
-            expect(myDoubleLink.toString()).toEqual([2, 'b', 'c', 'a']);
-            // myDoubleLink.delete(2);
-            // expect(myDoubleLink.toString()).toEqual(['b', 'c', 'a']);
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-x->2', 'x<-2->b', '2<-b->c', 'b<-c->0', 'c<-0->a', '0<-a->z', 'a<-z->null']);
+        });
+
+        it('can delete a node from the end', () => {
+            myDoubleLink.delete('z');
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-x->2', 'x<-2->b', '2<-b->c', 'b<-c->0', 'c<-0->a', '0<-a->null']);
+        });
+
+        it('can delete a node from the beginning', () => {
+            myDoubleLink.delete('x');
+            expect(myDoubleLink.toExplicitArray()).toEqual(
+                ['null<-2->b', '2<-b->c', 'b<-c->0', 'c<-0->a', '0<-a->null']);
         });
     });
 });
--- challenges/linked-list/doubly-linked-list/doubly-linked-list.js
+++ challenges/linked-list/doubly-linked-list/doubly-linked-list.js
@@ -27,30 +27,52 @@ class DoublyLinkedList {
         return false;
     }
 
-    toString() {
+    toExplicitArray() {
         let thisNode = this.head;
-        let valuesStringed = [];
+        let nodeArray = [];
+
         while(thisNode){
-            valuesStringed.push(thisNode.value);
+            //Would this be a place for a ternary? Not a big fan of them, but this seems like it could turn two lines into one.
+            let prev = null;
+            if(thisNode.previous) prev = thisNode.previous.value;
+            
+            let value = thisNode.value;
+
+            let next = null;
+            if(thisNode.next) next = thisNode.next.value;
+
+            nodeArray.push(`${prev}<-${value}->${next}`);
             thisNode = thisNode.next;
         }
-        return valuesStringed;
+        
+        return nodeArray;
     }
 
     append(value) {
         const appendedNode = new Node(value);
         let thisNode = this.head;
+        
         while(thisNode.next) thisNode = thisNode.next;
+        
         appendedNode.next = null;
+        appendedNode.previous = thisNode;
         thisNode.next = appendedNode;
     }
 
     insertBefore(value, newValue) {
         const insertedNode = new Node(newValue);
         let thisNode = this.head;
+
         while(thisNode.value !== value) thisNode = thisNode.next;
-        insertedNode.previous = thisNode.previous;
-        thisNode.previous.next = insertedNode; 
+
+        if(thisNode.previous) {
+            insertedNode.previous = thisNode.previous;
+            thisNode.previous.next = insertedNode;
+        } else {
+            this.head = insertedNode;
+            insertedNode.previous = null;
+        }
+
         thisNode.previous = insertedNode;
         insertedNode.next = thisNode;
     }
@@ -58,18 +80,37 @@ class DoublyLinkedList {
     insertAfter(value, newValue) {
         const insertedNode = new Node(newValue);
         let thisNode = this.head;
+
         while(thisNode.value !== value) thisNode = thisNode.next;
+        
+        
+        if(thisNode.next) {
+            insertedNode.next = thisNode.next;
+            thisNode.next.previous = insertedNode;
+        } else {
+            insertedNode.next = null;
+        }
+
         insertedNode.previous = thisNode;
         insertedNode.next = thisNode.next;
-        thisNode.next.previous = insertedNode;
+
         thisNode.next = insertedNode;
     }
 
     delete(value) {
         let thisNode = this.head;
+
         while(thisNode.value !== value) thisNode = thisNode.next;
-        thisNode.previous.next = thisNode.next;
-        thisNode.next.previous = thisNode.previous;
+
+        if(thisNode.next && thisNode.previous) {
+            thisNode.previous.next = thisNode.next;
+            thisNode.next.previous = thisNode.previous;
+        } else if(!thisNode.next && thisNode.previous) {
+            thisNode.previous.next = null;
+        } else if(thisNode.next && !thisNode.previous) {
+            thisNode.next.previous = null;
+            this.head = thisNode.next;
+        }
     }
 }
 
