08a77fff4d5e7125239bed5fa61320fd3a9d44d7
joelpdurham <joeldur@gmail.com>
2019-12-09 17:29:13 -0800

    wrote test for delete function

--- challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
+++ challenges/linked-list/doubly-linked-list/doubly-linke-list.test.js
@@ -35,6 +35,12 @@ describe('Doubly Linked-List tests', () => {
             myDoubleLink.insertAfter(1, 'c');
             expect(myDoubleLink.toString()).toEqual([2, 'b', 1, 'c', 0, 'a']);
         });
+        it('can delete a node given a value', () => {
+            myDoubleLink.delete(1);
+            //myDoubleLink.delete(2);
+            //myDoubleLink.delete(0);
+            expect(myDoubleLink.toString()).toEqual([2, 'b', 'c', 0, 'a']);
+        });
     });
 });
 
