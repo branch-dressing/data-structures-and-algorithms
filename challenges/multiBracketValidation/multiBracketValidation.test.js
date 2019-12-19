3b7e328028b40a24539f83b80c219c21915b0923
joelpdurham <joeldur@gmail.com>
2019-12-18 16:41:29 -0800

    wrote failing test

---
+++ challenges/multiBracketValidation/multiBracketValidation.test.js
@@ -0,0 +1,9 @@
+const { multiBracketValidation } = require('./multiBracketValidation');
+
+describe('Multi Bracket Validation', () => {
+    it('can validate simple examples', () => {
+        expect(multiBracketValidation('()')).toEqual(true);
+    
+    });
+});
+
