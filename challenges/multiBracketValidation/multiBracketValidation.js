d62db01514bad42761b52de93f8893cb8cba6fc6
joelpdurham <joeldur@gmail.com>
2019-12-18 17:24:33 -0800

    passed first test... but Im skeptic...

---
+++ challenges/multiBracketValidation/multiBracketValidation.js
@@ -0,0 +1,26 @@
+function multiBracketValidation(string) {
+    //let open = /[\(\{\[]/g;
+    // let close = /[\)\}\]]/g;
+    // let holdMyOpeners = string.match(open);
+    // let holdMyClosers = string.match(close);
+    // for(let i = holdMyOpeners.length; i > 0 ; i--) {
+    // }
+    let chars = string.split('');
+    let openArray = [];
+    
+    for(let i = 0; i < chars.length; i++) {
+        if(chars[i] === '(' || 
+            chars[i] === '{' || 
+            chars[i] === '[') openArray.push(chars[i]);
+
+        if(chars[i] === ')' && openArray.pop() !== '(') return false;
+        if(chars[i] === '}' && openArray.pop() !== '(') return false;
+        if(chars[i] === ']' && openArray.pop() !== '(') return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    multiBracketValidation
+};
