f4262f6594ee8ae4b9fbaf257aebdcfdac9fb2d8
joelpdurham <joeldur@gmail.com>
2020-02-03 14:46:44 -0800

    fixed indent issues, and prev issue

--- challenges/arrayShift/array-shift.test.js
+++ challenges/arrayShift/array-shift.test.js
@@ -1,22 +1,22 @@
 const {
-    insertShiftArray,
-    removeMiddle
+  insertShiftArray,
+  removeMiddle
 } = require('./array-shift.js');
 
 describe('Shift Array Test', () => {
-    it('takes an array and a value to be insert and inserts it into the middle', () => {
-        expect(insertShiftArray([], 100)).toEqual([100]);
-        expect(insertShiftArray([1], 2)).toEqual([1, 2]);
-        expect(insertShiftArray([1, 3], 2)).toEqual([1, 2, 3]);
-        expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
-        expect(insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
-    });
+  it('takes an array and a value to be insert and inserts it into the middle', () => {
+    expect(insertShiftArray([], 100)).toEqual([100]);
+    expect(insertShiftArray([1], 2)).toEqual([1, 2]);
+    expect(insertShiftArray([1, 3], 2)).toEqual([1, 2, 3]);
+    expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
+    expect(insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
+  });
 
-    it('removes the middle element of an array', () => {
-        expect(removeMiddle([])).toEqual('Nothing to remove');
-        expect(removeMiddle([1])).toEqual([]);
-        expect(removeMiddle([1, 2, 3])).toEqual([1, 3]);
-        expect(removeMiddle([2, 4, 5, 6, 8])).toEqual([2, 4, 6, 8]);
-        expect(removeMiddle([4, 8, 15, 16, 23, 42])).toEqual([4, 8, 16, 23, 42]);
-    });
+  it('removes the middle element of an array', () => {
+    expect(removeMiddle([])).toEqual('Nothing to remove');
+    expect(removeMiddle([1])).toEqual([]);
+    expect(removeMiddle([1, 2, 3])).toEqual([1, 3]);
+    expect(removeMiddle([2, 4, 5, 6, 8])).toEqual([2, 4, 6, 8]);
+    expect(removeMiddle([4, 8, 15, 16, 23, 42])).toEqual([4, 8, 16, 23, 42]);
+  });
 });
--- challenges/multiBracketValidation/multiBracketValidation.test.js
+++ challenges/multiBracketValidation/multiBracketValidation.test.js
@@ -1,22 +1,22 @@
 const { multiBracketValidation } = require('./multiBracketValidation');
 
 describe('Multi Bracket Validation', () => {
-    it('can validate simple examples', () => {
-        expect(multiBracketValidation('()')).toEqual(true);
-        expect(multiBracketValidation('[]')).toEqual(true);
-        expect(multiBracketValidation('{}')).toEqual(true);
-        expect(multiBracketValidation('[')).toEqual(false);
-        expect(multiBracketValidation(']')).toEqual(false);
-    });
+  it('can validate simple examples', () => {
+    expect(multiBracketValidation('()')).toEqual(true);
+    expect(multiBracketValidation('[]')).toEqual(true);
+    expect(multiBracketValidation('{}')).toEqual(true);
+    expect(multiBracketValidation('[')).toEqual(false);
+    expect(multiBracketValidation(']')).toEqual(false);
+  });
 
-    it('can validate the examples from code challenge', () => {
-        expect(multiBracketValidation('{}(){}')).toEqual(true);
-        expect(multiBracketValidation('()[[Extra Characters]]')).toEqual(true);
-        expect(multiBracketValidation('(){}[[]]')).toEqual(true);
-        expect(multiBracketValidation('{}{Code}[Fellows](())')).toEqual(true);
-        expect(multiBracketValidation('[({}]')).toEqual(false);
-        expect(multiBracketValidation('(](')).toEqual(false);
-        expect(multiBracketValidation('{(})')).toEqual(false);
-    });
+  it('can validate the examples from code challenge', () => {
+    expect(multiBracketValidation('{}(){}')).toEqual(true);
+    expect(multiBracketValidation('()[[Extra Characters]]')).toEqual(true);
+    expect(multiBracketValidation('(){}[[]]')).toEqual(true);
+    expect(multiBracketValidation('{}{Code}[Fellows](())')).toEqual(true);
+    expect(multiBracketValidation('[({}]')).toEqual(false);
+    expect(multiBracketValidation('(](')).toEqual(false);
+    expect(multiBracketValidation('{(})')).toEqual(false);
+  });
 });
 
