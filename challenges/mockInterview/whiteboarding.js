84cc1b3c19c7073fe352354ac0950e4f454afc30
joelpdurham <joeldur@gmail.com>
2020-02-03 14:49:30 -0800

    fixed final three issues...

--- challenges/mockInterview/whiteboarding.js
+++ challenges/mockInterview/whiteboarding.js
@@ -4,7 +4,7 @@ function matrixRowsSummed(matrix) {
   for(let i = 0; i < matrix.length; i++) {
     let acc = 0;
     for(let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === null) matrix[i][j] = 0;
+      if(matrix[i][j] === null) matrix[i][j] = 0;
       acc = acc + matrix[i][j];
     }
     arrayWithSummedRows[i] = acc;
--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -90,4 +90,4 @@ class BinaryTree {
 module.exports = {
   Node,
   BinaryTree
-}
+};
